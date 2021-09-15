7da103b01188c879f5d5e20888326e983c67f6b8
Xerronn <zonisick@gmail.com>
2021-09-15 11:48:22 -0400

    Added main memory declaration

--- src/administrators/declarations/memory.d.ts
+++ src/administrators/declarations/memory.d.ts
@@ -4,3 +4,23 @@ interface CreepMemory {
     spawnRoom: string;
     body: string[];
 }
+
+interface Memory {
+    creeps: {[creepName: string]: CreepMemory}
+    gFlags: {[flagName: string]: string | boolean};
+    rooms: {[roomName: string]: any};
+    scheduler: {
+        [roomName: string]: {
+            [tick: number]: {
+                [taskId: string]: {
+                    script: string,
+                    objArr: [
+                        string[],
+                        string,
+                        object
+                    ]
+                }
+            }
+        }
+    };
+}
