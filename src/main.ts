5b20b24347203ee3381039d342ddf4a570d3a13b
Xerronn <zonisick@gmail.com>
2021-09-19 23:55:21 -0400

    Added some more administrators into the global scope

--- src/main.ts
+++ src/main.ts
@@ -1,35 +1,15 @@
-import { ErrorMapper } from "thirdParty/ErrorMapper";
+import { Archivist } from "./administrators/archivist";
+import { ErrorMapper } from "./thirdParty/ErrorMapper";
 import { Imperator } from './administrators/imperator';
-
 declare global {
-    /*
-    Example types, expand on these or remove them and add your own.
-    Note: Values, properties defined here do no fully *exist* by this type definition alone.
-          You must also give them an implementation if you would like to use them. (ex. actually setting a `role` property in a Creeps memory)
-
-    Types added in this `global` block are in an ambient, global context. This is needed because `main.ts` is a module file (uses import or export).
-    Interfaces matching on name from @types/screeps will be merged. This is how you can extend the 'built-in' interfaces from @types/screeps.
-  */
-    // Memory extension samples
-    interface Memory {
-        uuid: number;
-        log: any;
-    }
-
-    interface CreepMemory {
-        role: string;
-        room: string;
-        working: boolean;
-    }
-
-    // Syntax for adding properties to `global` (ex "global.log")
     namespace NodeJS {
         interface Global {
             Imperator: Imperator;
+            Archivist: Archivist;
+            logger: boolean;
         }
     }
 }
-
 global.Imperator = new Imperator();
 
 // When compiling TS to JS and bundling with rollup, the line numbers and file names in error messages change
