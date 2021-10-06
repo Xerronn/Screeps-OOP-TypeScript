dd40d6fa4bf9af748c0d1d949b15bb22d8bd7049
Xerronn <zonisick@gmail.com>
2021-10-06 01:10:25 -0400

    Renamed enum declaration file to just types

--- src/administrators/declarations/types.d.ts
+++ src/administrators/declarations/types.d.ts
@@ -7,10 +7,14 @@ declare enum CASTRUM_TYPES {
     UNDEFINED = 'undefined'
 }
 
+declare type CastrumType =
+    'bastion' | 'conduit' | 'market' | 'nexus' | 'workshop' | 'undefined';
+
 declare enum CIVITAS_TYPES {
     ARBITER = 'arbiter',
     CHEMIST = 'chemist',
     CONTRACTOR = 'contractor',
+    COURIER = 'courier',
     CURATOR = 'curator',
     EMISSARY = 'emissary',
     EXCAVATOR = 'excavator',
@@ -20,11 +24,18 @@ declare enum CIVITAS_TYPES {
     SCOUT = 'scout',
 }
 
+declare type CivitasType =
+    'arbiter' | 'chemist' | 'contractor' | 'courier' | 'curator' |
+    'emissary' | 'excavator' | 'miner' | 'runner' | 'scholar' | 'scout';
+
 declare enum LEGION_TYPES {
     EXECUTIONER = 'executioner',
     GARRISON = 'garrison',
     JESTER = 'jester',
 }
+
+declare type LegionType =
+    'executioner' | 'garrison' | 'jester';
 
 declare enum REMOTE_STATUSES {
     SAFE = 'safe',
