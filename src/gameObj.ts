7fef7c0b84fa1c6d32ec466edab842c238f1665d
Xerronn <zonisick@gmail.com>
2021-10-10 03:09:58 -0400

    Added getters for executive and supervisor

--- src/gameObj.ts
+++ src/gameObj.ts
@@ -1,15 +1,26 @@
+import { Executive } from "administrators/executive";
+import { Supervisor } from "administrators/supervisor";
+
 export abstract class GameObj {
     abstract id: Id<any>;
-    abstract liveObj: Creep | Structure | null;
-    abstract pos: RoomPosition | null;
-    abstract room: string | null;
-    abstract hits: number | null;
-    abstract hitsMax: number | null;
+    abstract liveObj: Creep | Structure
+    abstract pos: RoomPosition
+    abstract room: string
+    abstract hits: number
+    abstract hitsMax: number
 
     abstract update(): boolean;
     abstract run(): boolean;
 
     info() {
         return `${this.constructor.name} with ID ${this.id}`;
+    }
+
+    get supervisor(): Supervisor {
+        return global.Imperator.administrators[this.room].supervisor;
+    }
+
+    get executive(): Executive {
+        return global.Imperator.administrators[this.room].executive;
     }
 }
