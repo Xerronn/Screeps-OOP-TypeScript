1c66fdb700c114c6761b70c295c38df3a896b472
Xerronn <zonisick@gmail.com>
2021-10-11 01:07:37 -0400

    Added Traveler in Typescript along with my added features

---
+++ src/thirdParty/travelerTypes.d.ts
@@ -0,0 +1,61 @@
+ /**
+     * TRAVELER STUFF
+     */
+  interface PathfinderReturn {
+    path: RoomPosition[];
+    ops: number;
+    cost: number;
+    incomplete: boolean;
+}
+
+interface TravelToReturnData {
+    nextPos?: RoomPosition;
+    pathfinderReturn?: PathfinderReturn;
+    state?: TravelState;
+    path?: string;
+}
+
+interface TravelToOptions {
+    allowSwap?: boolean;
+    ignoreRoads?: boolean;
+    ignoreCreeps?: boolean;
+    ignoreStructures?: boolean;
+    preferHighway?: boolean;
+    highwayBias?: number;
+    allowSK?: boolean;
+    range?: number;
+    obstacles?: {pos: RoomPosition}[];
+    roomCallback?: (roomName: string, matrix: CostMatrix) => CostMatrix | boolean;
+    routeCallback?: (roomName: string) => number;
+    returnData?: TravelToReturnData;
+    restrictDistance?: number;
+    useFindRoute?: boolean;
+    maxOps?: number;
+    movingTarget?: boolean;
+    freshMatrix?: boolean;
+    offRoad?: boolean;
+    stuckValue?: number;
+    maxRooms?: number;
+    repath?: number;
+    route?: {[roomName: string]: boolean};
+    ensurePath?: boolean;
+}
+
+interface TravelData {
+    state: any[];
+    path?: string;
+}
+
+interface TravelState {
+    stuckCount: number;
+    lastCoord: Coord;
+    destination: RoomPosition;
+    cpu: number;
+}
+
+interface Creep {
+    travelTo(destination: HasPos|RoomPosition, ops?: TravelToOptions): number;
+}
+
+type Coord = {x: number, y: number};
+type HasPos = {pos: RoomPosition}
