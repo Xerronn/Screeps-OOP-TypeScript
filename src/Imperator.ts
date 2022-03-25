d98604365954c2fd756097cd95503bd16484ee13
Xerronn <zonisick@gmail.com>
2022-03-24 22:17:03 -0400

    updated imports

--- src/Imperator.ts
+++ src/Imperator.ts
@@ -1,7 +1,7 @@
-import Supervisor from './Supervisor';
-import Executive from './Executive';
-import Chronicler from '../controllers/Chronicler';
-import Architect from '../controllers/Architect';
+import Supervisor from './administrators/Supervisor';
+import Executive from './administrators/Executive';
+import Chronicler from './controllers/Chronicler';
+import Architect from './controllers/Architect';
 
 //highest level class overseeing all operations in the game
 export default class Imperator {
