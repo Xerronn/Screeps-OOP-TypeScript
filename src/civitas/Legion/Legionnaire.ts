190bd041b7d9ead1ccb58fc5feeab1c4092cb6fd
Xerronn <zonisick@gmail.com>
2022-04-14 14:22:07 -0400

    Basic killer creep implementation

--- src/civitas/Legion/Legionnaire.ts
+++ src/civitas/Legion/Legionnaire.ts
@@ -1,5 +1,35 @@
 import Civitas from '../civitas'
 
 export default class Legionnaire extends Civitas {
+    constructor(emissary: Creep) {
+        super(emissary);
+    }
 
+    run() {
+        if (!this.arrived) {
+            return this.march(this.assignedRoom);
+        }
+
+        let target = Game.rooms[this.room].find(FIND_HOSTILE_SPAWNS)[0];
+        this.kill(target);
+        return;
+    }
+
+    /**
+     * Method to flee to home room when there is an invader or enemy
+     */
+    flee() {
+        this.march(this.memory.spawnRoom);
+    }
+
+    /**
+     * Method that travels to the room controller and reserves it
+     */
+    kill(spawn: StructureSpawn) {
+        if (this.pos.inRangeTo(spawn, 1)) {
+            this.liveObj.attack(spawn);
+        } else {
+            this.liveObj.travelTo(spawn);
+        }
+    }
 }
